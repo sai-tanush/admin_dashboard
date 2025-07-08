9c7095cbcc7466a41fdbaa8e6e1db3218a7fd734
Sai Tanush <saitanush@Sais-MacBook-Pro.local>
2025-07-08 18:26:54 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="w-screen h-screen bg-gray-700 flex flex-col justify-center items-center">
+      <p className="text-4xl font-semibold text-white">Responsive Admin Dashboard</p>
+    </div>
+  )
+}
+
+export default App
