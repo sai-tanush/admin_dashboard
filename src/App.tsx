0ecb8333e478bf6d116db4827ab234b444c323ac
Sai Tanush - Personal <saitanush789@gmail.com>
2025-07-09 12:01:22 +0530

    chore: insatlled button component from shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./components/ui/button"
 
 function App() {
 
@@ -5,7 +6,8 @@ function App() {
     <div className="w-screen h-screen bg-background flex flex-col justify-center items-center gap-4">
       <h1 className="text-4xl font-semibold text-primaryBlack">Responsive Admin Dashboard</h1>
       <p className="text-2xl font-medium text-gray-800">Whereas disregard and contempt for human rights have resulted</p>
-      <button className="mt-12 py-2 px-12 text-xl bg-primary text-gray-800 border border-secondary rounded-lg cursor-pointer hover:bg-secondary">Button</button>
+      {/* <button className="mt-12 py-2 px-12 text-xl bg-primary text-gray-800 border border-secondary rounded-lg cursor-pointer hover:bg-secondary">Button</button> */}
+      <Button className="">Button</Button>
     </div>
   )
 }
