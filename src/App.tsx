e1db0d227b917e37c82023dfaaedda1857312c9e
Sai Tanush - Personal <saitanush789@gmail.com>
2025-07-10 07:37:49 +0530

    feat: updated app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { Button } from "./components/ui/button"
+import Layout from "./components/layout"
+
 
 function App() {
 
   return (
-    <div className="w-screen h-screen bg-background flex flex-col justify-center items-center gap-4">
-      <h1 className="text-4xl font-semibold text-primaryBlack">Responsive Admin Dashboard</h1>
-      <p className="text-2xl font-medium text-gray-800">Whereas disregard and contempt for human rights have resulted</p>
-      {/* <button className="mt-12 py-2 px-12 text-xl bg-primary text-gray-800 border border-secondary rounded-lg cursor-pointer hover:bg-secondary">Button</button> */}
-      <Button className="">Button</Button>
+    <div className="w-screen min-h-screen bg-background">
+        <Layout />
     </div>
   )
 }
